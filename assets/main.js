24c804c7d945ad7f4e63502dd9a1d268e9084d43
xilesun <2013xile@gmail.com>
2016-10-06 18:55:17 +0800

    float-demo

--- assets/main.js
+++ assets/main.js
@@ -52,13 +52,12 @@
 	
 	var _jquery2 = _interopRequireDefault(_jquery);
 	
-	var _demo = __webpack_require__(2);
+	var _float = __webpack_require__(2);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	(0, _jquery2.default)(document).ready(function ($) {
-	  (0, _demo.display)();
-	  $(window).on('resize', _demo.display);
+		$(window).resize(_float.display);
 	});
 
 /***/ },
@@ -78,45 +77,20 @@
 	});
 	exports.display = display;
 	function display() {
-		if ($("body").attr('class') == "table") {
-			var width = window.innerWidth;
-	
-			reDisplay();
-	
-			if (width > 1100) {
-				$("section:eq(0)>div").slice(0, 4).wrapAll("<div></div>");
-				$("section:eq(0)>div").slice(1, 3).wrapAll("<div></div>");
-				$("section:eq(0)>div:eq(1)").append("<div class='hide'>h</div>");
-				$("section:eq(0)>div:eq(1)").append("<div class='hide'>h</div>");
-				$("section:eq(4)>div").slice(0, 4).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(1, 3).wrapAll("<div></div>");
-				$("section:eq(4)>div:eq(1)").append("<div class='hide'>h</div>");
-				$("section:eq(4)>div:eq(1)").append("<div class='hide'>h</div>");
-			}
-			if (width < 1100 && width > 800) {
-				$("section:eq(0)>div").slice(0, 3).wrapAll("<div></div>");
-				$("section:eq(0)>div").slice(1, 4).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(0, 3).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(1, 4).wrapAll("<div></div>");
-			}
-			if (width < 800 && width > 500) {
-				$("section:eq(0)>div").slice(0, 2).wrapAll("<div></div>");
-				$("section:eq(0)>div").slice(1, 3).wrapAll("<div></div>");
-				$("section:eq(0)>div").slice(2, 4).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(0, 2).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(1, 3).wrapAll("<div></div>");
-				$("section:eq(4)>div").slice(2, 4).wrapAll("<div></div>");
-			}
-			if (width < 500) {
-				$("section:eq(0)>div").wrap("<div></div>");
-				$("section:eq(4)>div").wrap("<div></div>");
-			}
+		var width = window.innerWidth,
+		    left = $(".left"),
+		    right = $(".right");
+		if (width > 768) {
+			left.html("<div>1</div><div>2</div><div>5</div><div>6</div>");
+			right.html("<div>3</div><div>4</div><div>7</div>");
 		}
-	}
-	
-	function reDisplay() {
-		$("section:eq(0)").html("<div>1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>6</div>");
-		$("section:eq(4)").html("<div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem rem nam dolore repellendus provident, voluptas necessitatibus vel cupiditate delectus, doloremque incidunt accusantium quia! Nisi molestiae totam natus, in assumenda accusantium.</div></div><div><div>Lorem ipsum dolor sit amet.</div></div><div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At sunt harum ut rerum id quae voluptas velit iusto quasi distinctio.</div></div><div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, sequi?</div></div><div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime nisi deserunt, dolorem accusamus sint ipsam dolor quae ab animi assumenda architecto placeat possimus fugit doloribus vel, corporis amet aliquam maiores!</div></div><div><div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div>");
+		if (width < 768 && width > 420) {
+			left.html("<div>1</div><div>5</div><div>6</div>");
+			right.html("<div>3</div><div>2</div><div>4</div><div>7</div>");
+		}
+		if (width < 420) {
+			left.html("<div>3</div><div>2</div><div>1</div><div>4</div><div>5</div><div>6</div><div>7</div>");
+		}
 	}
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
