d125bbab0916aa1a53ead7956e50196cc93b8cc0
xilesun <2013xile@gmail.com>
2016-10-20 21:27:11 +0800

    editjs

--- src/scripts/square.js
+++ src/scripts/square.js
@@ -1,12 +1,19 @@
 function display(){
   var square = $("section>div"),
-      view = (window.innerWidth) * 0.9, //获取容器大小
       w = parseInt(square.css("width").replace("px", "")),//获取方块大小
-      gutter = 10,//设置间距
-      col = Math.floor(view / (w + gutter)),//获取列数
-      width = (col * w ) + (gutter * (col - 1)) + gutter;//计算section宽度
+      gutter = parseInt(square.css("margin").replace("px","")) * 2,//设置间距
+      top = $("section>div:eq(0)").offset().top,
+      view = window.innerWidth,
+      width = 0;
 
-      $("section").css("width", width)
+      $("section").css("width", view * 0.9);
+      for(var i = 0; i < square.length; i++){
+      	var t = $("section>div:eq(" + i + ")").offset().top;
+      	if(t !== top) break;
+        width = width + w + gutter;        
+      }
+      $("section").css("width", width);
+      console.log(view);
 }
 
 display();
